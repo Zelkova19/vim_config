2f3689f1951e7098d1a7ab791e2448f0ab6913a7
zelkova <cohiba666@gmail.com>
2026-01-25 22:42:21 +0300

    Initialize commit

---
+++ lua/plugins/onedark.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"navarasu/onedark.nvim",
+		version = "v0.1.0", -- Pin to legacy version
+		priority = 1000,
+		config = function()
+			require('onedark').setup({
+				style = 'darker',
+				transparent = true
+			})
+			require('onedark').load()
+		end
+	}
+}
