72806101ac0cc918995e15ad27c3629fd34bad2c
zelkova <cohiba666@gmail.com>
2026-02-05 17:27:55 +0300

    aaa

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -20,7 +20,13 @@ return {
 			},
 		})
 		require("neo-tree").setup({
-			close_if_last_window = false,
+			filesystem = {
+				filtered_items = {
+					visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+					hide_dotfiles = false,
+					hide_gitignored = true,
+				},
+			},
 		})
 	end,
 }
